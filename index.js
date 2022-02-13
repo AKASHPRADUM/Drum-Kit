28f7b232bec179ebbdbf2d5077d3a4c4445047b4
AKASHPRADUM <pradumkumardubey.email>
2022-02-13 15:09:44 +0530

    initial

---
+++ index.js
@@ -0,0 +1,61 @@
+var noOfButton = document.querySelectorAll(".drum").length;
+for (var i = 0; i < noOfButton; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttenInnerHTML = this.innerHTML;
+    playSound(buttenInnerHTML);
+    activeButton(buttenInnerHTML);
+  });
+
+}
+
+document.addEventListener("keypress", function(event){
+  playSound(event.key);
+  activeButton(event.key);
+  // console.log(event);
+})
+function playSound(key) {
+  switch (key) {
+    case "w":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "a":
+      var kickBass = new Audio("sounds/kick-bass.mp3");
+      kickBass.play();
+      break;
+    case "s":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "d":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "j":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "k":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "l":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    default:
+        console.log(buttenInnerHTML)
+      break;
+  }
+}
+
+function activeButton(pressedKey) {
+  var activeButtonKey = document.querySelector("."+pressedKey)
+  activeButtonKey.classList.add("pressed");
+  setTimeout(function(){
+    activeButtonKey.classList.remove("pressed");
+  },100)
+}
